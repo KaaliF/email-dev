431bf4738b1159bad1c099efb39e229e85ecd19c
Farhan Nawaz <fnawaz84@gmail.com>
2018-09-05 23:32:55 +0500

    google auth

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientSecret: "zBoyWvaOkYQat5A3kcIQ3Tg1",
+  googleClientID:
+    "397435637108-m7q15bcmrhut6gto3v97nv9s4kq8k9oo.apps.googleusercontent.com"
+};
+// clientsecret zBoyWvaOkYQat5A3kcIQ3Tg1
+// client id 397435637108-m7q15bcmrhut6gto3v97nv9s4kq8k9oo.apps.googleusercontent.com
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const keys = require('./config/keys');
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+    console.log(accessToken);
 
-app.get('/', (req, res) => {
-    res.send("ppp");
-});
+}));
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+app.get('/auth/google/callback', passport.authenticate('google', {
+
+}))
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
