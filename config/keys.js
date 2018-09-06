ca736a54cb224c8ef473376e6d70329ad725b832
Farhan Nawaz <fnawaz84@gmail.com>
2018-09-07 00:04:00 +0500

    google auth

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientSecret: "zBoyWvaOkYQat5A3kcIQ3Tg1",
-  googleClientID:
-    "397435637108-m7q15bcmrhut6gto3v97nv9s4kq8k9oo.apps.googleusercontent.com"
+  googleClientID: "397435637108-m7q15bcmrhut6gto3v97nv9s4kq8k9oo.apps.googleusercontent.com",
+  moongooseURI: "mongodb://<dbuser>:<dbpassword>@ds247852.mlab.com:47852/email-dev"
 };
 // clientsecret zBoyWvaOkYQat5A3kcIQ3Tg1
 // client id 397435637108-m7q15bcmrhut6gto3v97nv9s4kq8k9oo.apps.googleusercontent.com
--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const app = express();
-const keys = require('./config/keys');
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-    console.log(accessToken);
-
-}));
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
-app.get('/auth/google/callback', passport.authenticate('google', {
-
-}))
+const moongose = require('mongoose');
+const passportConfig = require('./services/passport');
+const MoongooseRequire = require('./config/keys')
+const app = express(MoongooseRequire.moongooseURI);
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
